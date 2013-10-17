db1d79d25648094084aeedcf500c762cdcd7e6ba
Developer <developer@crowdint.com>
2013-10-17 12:40:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleGraph::Application.config.secret_token = 'c3484d748980df58982eb5ba90dcab4428f28996859098b142d91fe6a88786c9404bf4b873c7a41c69c6e0e556c85da7451225eaf55a52f7d0509bd05017dd20'
